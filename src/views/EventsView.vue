aab44917615524edced29dd804ad399eff9743ee
Leandro Dzib <leandrodzibn@gmail.com>
2024-11-03 20:49:35 -0600

    Enhance EventsView component with Title and EventsList integration

--- src/views/EventsView.vue
+++ src/views/EventsView.vue
@@ -1,5 +1,27 @@
 <template>
-  <main>Events View</main>
+  <main>
+    <Title title="Eventos" />
+    <div class="container-events">
+      <EventsList />
+    </div>
+  </main>
 </template>
-<script setup lang="ts"></script>
-<style lang="scss" scoped></style>
+<script lang="ts">
+import EventsList from '@/components/events/EventsList.vue'
+import Title from '@/components/Title.vue'
+export default {
+  components: {
+    EventsList,
+    Title
+  }
+}
+</script>
+<style lang="scss" scoped>
+.container-events {
+  // align-items: start;
+  width: 100%;
+  background-color: #ede6de;
+  margin-bottom: 20px;
+  padding: 20px;
+}
+</style>
