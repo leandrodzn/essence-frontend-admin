d983b1f32a65d341a26205271234fcc7cc5ad26a
leandrodzn <leodzib100@gmail.com>
2024-11-17 08:46:15 -0600

    Integrate Vuetify plugin for enhanced UI components

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { createPinia } from 'pinia'
 
 import VueFeather from 'vue-feather'
 
+// Vuetify
+import vuetify from '@plugins/vuetify'
+
 import App from './App.vue'
 import router from './router'
 
@@ -12,6 +15,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.component(VueFeather.name as string, VueFeather)
 
